109c64bddfe414791de0a38bd2df9452a9788fea
Van Chi Hieu <vanchihieu3@gmail.com>
2024-08-28 14:39:51 +0700

    UI Carousel Homepage

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import "./App.css";
 import darkTheme from "./theme/DarkTheme";
 import NavBar from "./components/Navbar/Navbar";
+import HomePage from "./components/Home/HomePage";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <div>
-        <NavBar />
-      </div>
+      <NavBar />
+      <HomePage />
     </ThemeProvider>
   );
 }
