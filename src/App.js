b990591c2fce85155c9106f69d4510f55bdb3d97
Van Chi Hieu <vanchihieu3@gmail.com>
2024-08-29 21:32:08 +0700

    Update Auth Navbar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import "./App.css";
 import darkTheme from "./theme/DarkTheme";
 import Routers from "./Routers/Routers.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUser } from "./State/Authentication/Action";
 
 function App() {
+  const dispatch = useDispatch();
+  const { auth } = useSelector((store) => store);
+  const jwt = localStorage.getItem("jwt");
+
+  useEffect(() => {
+    if (jwt) {
+      dispatch(getUser(jwt));
+    }
+  }, [auth.jwt]);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
--- src/customers/components/Navbar/Navbar.jsx
+++ src/customers/components/Navbar/Navbar.jsx
@@ -6,29 +6,63 @@ import {
   Menu,
   MenuItem,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import SearchIcon from "@mui/icons-material/Search";
 import PersonIcon from "@mui/icons-material/Person";
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
 import Auth from "../../pages/Auth/Auth";
+import { useDispatch, useSelector } from "react-redux";
+import { pink } from "@mui/material/colors";
+import { logout } from "../../../State/Authentication/Action";
 
 const NavBar = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { auth } = useSelector((store) => store);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleOpenMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const navigateToCart = () => {
+    navigate("/cart");
+  };
+
+  const navigateToProfile = (e) => {
+    auth.user?.role === "ROLE_ADMIN" ||
+    auth.user?.role === "ROLE_RESTAURANT_OWNER"
+      ? navigate("/admin/restaurant")
+      : navigate("/my-profile");
+  };
 
   const handleCloseAuthModel = () => {
     navigate("/");
   };
+
   const navigateToHome = () => {
     navigate("/");
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    handleCloseMenu();
   };
 
   return (
     <div className="px-5 z-50 py-[.8rem] bg-[#e91e63]  lg:px-20 flex justify-between">
       <div className="flex items-center space-x-4">
         <div
-          // onClick={navigateToHome}
+          onClick={navigateToHome}
           className="flex items-center space-x-4 cursor-pointer lg:mr-10"
         >
           <li className="text-2xl font-semibold text-gray-300 logo">
@@ -44,23 +78,57 @@ const NavBar = () => {
             <SearchIcon sx={{ fontSize: "1.5rem" }} />
           </IconButton>
         </div>
+
         <div className="flex items-center space-x-2">
-          <IconButton onClick={() => navigate("/account/login")}>
-            <PersonIcon sx={{ fontSize: "2rem" }} />
-          </IconButton>
+          {auth.user?.fullName ? (
+            <span
+              id="demo-positioned-button"
+              aria-controls={open ? "demo-positioned-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={
+                auth.user?.role === "ROLE_ADMIN"
+                  ? handleOpenMenu
+                  : navigateToProfile
+              }
+              className=" font-semibold cursor-pointer"
+            >
+              <Avatar
+                sx={{ bgcolor: "white", color: pink.A400 }}
+                className="bg-white"
+              >
+                {auth.user.fullName[0].toUpperCase()}
+              </Avatar>
+            </span>
+          ) : (
+            <IconButton onClick={() => navigate("/account/login")}>
+              <PersonIcon sx={{ fontSize: "2rem" }} />
+            </IconButton>
+          )}
 
           <Menu
             id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleCloseMenu}
             MenuListProps={{
               "aria-labelledby": "basic-button",
             }}
           >
-            <MenuItem>Profile</MenuItem>
-            <MenuItem>Logout</MenuItem>
+            <MenuItem
+              onClick={() =>
+                auth.user?.role === "ROLE_ADMIN"
+                  ? navigate("/admin")
+                  : navigate("/super-admin")
+              }
+            >
+              Profile
+            </MenuItem>
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
           </Menu>
         </div>
 
-        <IconButton>
+        <IconButton onClick={navigateToCart}>
           <Badge color="black" badgeContent={3}>
             <ShoppingCartIcon className="text-4xl" sx={{ fontSize: "2rem" }} />
           </Badge>
