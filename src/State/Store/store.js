c8ef827992bb503ff887e197c40e95e8f423802c
Van Chi Hieu <vanchihieu3@gmail.com>
2024-08-31 17:18:56 +0700

    Reducer Cart redux

--- src/State/Store/store.js
+++ src/State/Store/store.js
@@ -3,11 +3,13 @@ import thunk from "redux-thunk";
 import authReducer from "../Authentication/Reducer";
 import restaurantReducer from "../Customers/Restaurant/Reducer";
 import menuItemReducer from "../Customers/Menu/Reducer";
+import cartReducer from "../Customers/Cart/Reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   restaurant: restaurantReducer,
   menu: menuItemReducer,
+  cart: cartReducer,
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
