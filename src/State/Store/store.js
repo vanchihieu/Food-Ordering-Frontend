3234890cfd1731ff159254e48661f44c2055dd50
Van Chi Hieu <vanchihieu3@gmail.com>
2024-08-31 16:50:10 +0700

    Action Types Cart redux

--- src/State/Store/store.js
+++ src/State/Store/store.js
@@ -2,10 +2,12 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import authReducer from "../Authentication/Reducer";
 import restaurantReducer from "../Customers/Restaurant/Reducer";
+import menuItemReducer from "../Customers/Menu/Reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   restaurant: restaurantReducer,
+  menu: menuItemReducer,
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
