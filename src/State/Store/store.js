5bdf8317a3a27f7cf4dd624e33fa8211d528a2d6
Van Chi Hieu <vanchihieu3@gmail.com>
2024-08-31 15:53:33 +0700

    Action type menu(food) redux

--- src/State/Store/store.js
+++ src/State/Store/store.js
@@ -1,9 +1,11 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import authReducer from "../Authentication/Reducer";
+import restaurantReducer from "../Customers/Restaurant/Reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  restaurant: restaurantReducer,
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
